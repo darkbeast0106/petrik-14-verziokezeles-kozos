4a47969fa60254060bb090d9925b5304931b3031
Budaházi Bence <budahazi.bence@petrik.hu>
2023-09-21 08:54:05 +0200

    Gomb színének módosítása - Pirosra

--- PetrikGitKozos/Program.cs
+++ PetrikGitKozos/Program.cs
@@ -11,7 +11,7 @@ namespace PetrikGitKozos
     {
         static void Main(string[] args)
         {
-            Color szin = Color.DarkBlue;
+            Color szin = Color.Red;
             string szoveg = "Meglepetés!";
 
             Form1 f = new Form1(szin, szoveg);
